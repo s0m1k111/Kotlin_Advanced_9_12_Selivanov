f439cede3a9e0531ef06ec7b11149d2bc1ac1a50
s0m1k111 <spavel1207@gmail.com>
2026-01-14 10:52:36 +0300

    Add OutpostWorker with getters/setters

---
+++ src/OutpostWorker.kt
@@ -0,0 +1,53 @@
+import kotlin.math.max
+
+class OutpostWorker(val name: String) {
+
+    var maxEnergy: Int = 100
+        private set
+
+    var energy: Int = maxEnergy
+        set(value){
+            field = value.coerceIn(0,maxEnergy)
+        }
+
+    var mood: Int = 50
+        get() = field + (energy / 10)
+
+    var level: Int = 1
+        private set
+
+    fun work(){
+        println("$name выполняет работу...")
+        energy -= 15
+        if(energy < 20) println("$name устал!")
+    }
+
+    fun rest(){
+        println("$name отдыхает...")
+        energy += 20
+    }
+
+    fun levelUp(){
+        level++
+        maxEnergy += 20
+        energy = maxEnergy
+        println("$name повышает уровень: $level |" +
+                "Максимальная энергия увеличена до: $maxEnergy")
+    }
+
+
+}
+fun main(){
+    val worker = OutpostWorker("KKK")
+    println("Energy: ${worker.energy}")
+    println("Vibe: ${worker.mood}")
+    worker.work()
+    worker.work()
+    worker.work()
+    println("Energy: ${worker.energy}")
+    println("Vibe: ${worker.mood}")
+    /*worker.energy = 150
+    worker.level = 5*/
+    worker.levelUp()
+
+}
